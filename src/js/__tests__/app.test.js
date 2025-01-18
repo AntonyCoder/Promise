4b61fa999fa0778b22198b3acced5f176dbe9ba6
Anton Lysenko <antonlysenko161@yandex.ru>
2025-01-18 18:29:07 +0300

    HW done

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,80 @@
+import GameSavingLoader from "../app";
+import read from "../reader";
+import json from "../parser";
+import GameSaving from "../GameSaving";
+
+jest.mock('../parser.js');
+jest.mock('../reader.js');
+
+describe('GameSavingLoader', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    })
+
+    test('Корректно создается GameSaving', (done) => {
+        const mockBuffer = new ArrayBuffer(16);
+        const mockJson = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}';
+
+        read.mockResolvedValue(mockBuffer);
+        json.mockResolvedValue(mockJson);
+
+        GameSavingLoader.load().then((result) => {
+            expect(result).toBeInstanceOf(GameSaving)
+            expect(result).toEqual(
+                new GameSaving(9, 1546300800, {
+                    id: 1,
+                    name: 'Hitman',
+                    level: 10,
+                    points: 2000,
+                })
+            );
+
+            expect(read).toHaveBeenCalledTimes(1);
+            expect(json).toHaveBeenCalledWith(mockBuffer);
+
+            done();
+        })
+            .catch((err) => done(err));
+    })
+
+    test('обработка ошибок read', (done) => {
+        const mockError = new Error('Ошибка');
+        read.mockRejectedValue(mockError);
+
+        GameSavingLoader.load().then(() => {
+            done(new Error('Ожидалась ошибка но не произошла'))
+        }).catch((error) => {
+            expect(error).toBe(mockError)
+            done()
+        })
+    })
+
+    test('должен обрабатывать ошибки из json', (done) => {
+        const mockBuffer = new ArrayBuffer(16);
+        const mockError = new Error('Ошибка парсинга');
+
+        read.mockResolvedValue(mockBuffer);
+        json.mockRejectedValue(mockError);
+
+        GameSavingLoader.load()
+            .then(() => {
+                done(new Error('Ожидалась ошибка, но её не произошло'));
+            })
+            .catch((error) => {
+                expect(error).toBe(mockError);
+                done();
+            });
+    });
+})
+
+
+
+
+// test('Проверка работы загрузчика', () => {
+//     const result = new GameSavingLoader().load().then((result) => {
+//         return result
+//     })
+//     const data = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}'
+//     expect(result).toEqual(data);
+// })
+
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,14 @@
+import read from "./reader";
+import json from "./parser";
+import GameSaving from "./GameSaving";
 
+export default class GameSavingLoader {
+    static load() {
+        return read()
+            .then((data) => json(data))
+            .then((parsedData) => {
+                const { id, created, userInfo } = JSON.parse(parsedData);
+                return new GameSaving(id, created, userInfo);
+            })
+    }
+}
