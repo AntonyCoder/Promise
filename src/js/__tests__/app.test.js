051c858ba8a9199901fba484d18f738599c8e3da
Anton Lysenko <antonlysenko161@yandex.ru>
2025-01-18 18:41:57 +0300

    Fix some mistake

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -66,15 +66,3 @@ describe('GameSavingLoader', () => {
             });
     });
 })
-
-
-
-
-// test('Проверка работы загрузчика', () => {
-//     const result = new GameSavingLoader().load().then((result) => {
-//         return result
-//     })
-//     const data = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}'
-//     expect(result).toEqual(data);
-// })
-
